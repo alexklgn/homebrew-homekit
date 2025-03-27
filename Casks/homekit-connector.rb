5e833ab7ba0919be1c5241b418845317a60c48c0
Alex Kulagin <alexkulagin@icloud.com>
2025-03-27 05:13:58 +0000

    Update homekit-connector to v1.0.1

--- Casks/homekit-connector.rb
+++ Casks/homekit-connector.rb
@@ -1,8 +1,8 @@
 cask "homekit-connector" do
-  version "1.0.0"
-  sha256 "1a2bc7a8ce42724f2049ba9cf744a1889f1206b5dc22adc88f49189000186a6d"
+  version "1.0.1"
+  sha256 "c86b9bccd199cc8b3bdf181fa9907b79b34bc52f2210313119483813a8ee0cb1"
 
-  url "https://github.com/alexklgn/homebrew-homekit/raw/main/HomekitConnector.dmg"
+  url "https://github.com/alexklgn/HomekitConnector/releases/download/v#{version}/HomekitConnector.dmg"
   name "HomekitConnector"
   desc "A macOS menubar app for HomeKit control"
   homepage "https://github.com/alexklgn/HomekitConnector"
